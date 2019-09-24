ca17a0dbe17e09b3af8dbd92c6542b05a3b6a6f4
gtsofa <jlsnyule@yahoo.com>
2019-09-24 23:28:44 +0300

    loading text files

--- test.rb
+++ test.rb
@@ -1,10 +1,7 @@
-require 'sinatra'
-
 def page_content(title)
   File.read("pages/#{title}.txt")
 rescue Errno::ENOENT
   return nill
 end
-get '/' do
-  erb :welcome
-end
+
+puts page_content("TsofaNyule")
