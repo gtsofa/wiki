af19d5647924f27bff85c356ae5b1e6c4d0b77d9
gtsofa <jlsnyule@yahoo.com>
2019-09-25 23:13:40 +0300

    add a new route

--- wiki.rb
+++ wiki.rb
@@ -5,12 +5,24 @@ def page_content(title)
 rescue Errno::ENOENT
   return nill
 end
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
 get '/' do
   erb :welcome
 end
+
+get '/new' do
+  erb :new
+end
+
 
 get '/:title' do
   @title = params[:title]
   @content = page_content(@title)
   erb :show
 end
+
